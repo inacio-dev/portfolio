56f1370faec1080daee6e748392411be537b1c34
Darkissons <inaciormgalvao@outlook.com>
2023-04-07 18:10:34 -0300

    fix: Language fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,21 @@
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 import RootPage from './routes/root'
 import i18n from './config'
+import HomePage from './routes/home'
 
 export default function App() {
+  const setLanguage = (lang: string) => {
+    i18n.changeLanguage(lang)
+  }
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route
-          path="/:lang/*"
-          element={
-            <RootPage
-              setLanguage={(lang: string) => {
-                i18n.changeLanguage(lang)
-              }}
-            />
-          }
-        />
-        <Route
-          path="/"
-          element={
-            <RootPage
-              setLanguage={(lang: string) => {
-                i18n.changeLanguage(lang)
-              }}
-            />
-          }
-        />
-        <Route path="*" element={<Navigate to="/" />} />
+        <Route path="/:lang/*" element={<RootPage setLanguage={i18n.changeLanguage} />}>
+          <Route index element={<HomePage />} />
+        </Route>
+
+        <Route path="/" element={<Navigate to={`/${i18n.language}/`} replace />} />
       </Routes>
     </BrowserRouter>
   )
