c2d82e9d3d004e63d297679340abacae3f64d6dd
Darkissons <inaciormgalvao@outlook.com>
2023-04-07 17:38:06 -0300

    Language fix

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,7 +8,7 @@ export default function Header({ setLanguage }: HeaderProps) {
   const { t } = useTranslation()
   return (
     <header className="flex">
-      <button onClick={() => setLanguage(`en-us`)}>{t('home.text')}</button>
+      <button onClick={() => setLanguage('en-US')}>{t('home.text')}</button>
     </header>
   )
 }
