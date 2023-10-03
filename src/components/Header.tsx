df66938da6c909d9a1184986a87e68cfa718a0f7
inacio-dev <inaciormgalvao@gmail.com>
2023-10-03 10:18:56 -0300

    header-mobile

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,10 @@
 import { useLocale } from 'next-intl'
 import { getTranslator } from 'next-intl/server'
 
-import HeaderLinks from './HeaderLinks'
 import LanguageSwitcher from './LanguageSwitcher'
 import LogoAnimation from './LogoAnimation'
+import Navigation from './Navigation'
+import NavigationMobile from './NavigationMobile'
 import ThemeSwitcher from './ThemeSwitcher'
 
 export default async function Header() {
@@ -17,7 +18,7 @@ export default async function Header() {
         <LogoAnimation title={t('title')} />
 
         <div className="z-10 flex h-full w-fit flex-row items-center justify-end space-x-3 overflow-hidden rounded-l-md bg-brand-mindaro pl-3 pr-[16px] transition-all duration-500 dark:bg-brand-indigo sm:pr-[33px] md:pr-28 md:shadow-md xl:pr-[16vw]">
-          <HeaderLinks />
+          <Navigation />
 
           <ThemeSwitcher />
 
@@ -25,9 +26,7 @@ export default async function Header() {
         </div>
       </div>
 
-      <div className="flex h-fit w-full items-center justify-center bg-brand-mindaro text-slate-light-3 shadow-md transition-all duration-500 dark:bg-brand-indigo md:hidden">
-        123
-      </div>
+      <NavigationMobile />
     </header>
   )
 }
---
+++ src/components/NavigationMobile.tsx
@@ -0,0 +1,47 @@
+'use client'
+
+import { useEffect, useState } from 'react'
+import { motion, useAnimation } from 'framer-motion'
+
+import useLocalStorage from '../hooks/useLocalStorage'
+import NavLinks from './NavLinks'
+
+export default function NavigationMobile() {
+  const [visibleHeaderMobile] = useLocalStorage('header-mobile', false)
+  const controlsMenu = useAnimation()
+  const controlsLinks = useAnimation()
+
+  const [isHiddenLinks, setIsHiddenLinks] = useState(true)
+
+  useEffect(() => {
+    setTimeout(
+      () => {
+        setIsHiddenLinks(!visibleHeaderMobile)
+      },
+      visibleHeaderMobile ? 300 : 50,
+    )
+
+    controlsLinks.start(visibleHeaderMobile ? 'visible' : 'hidden')
+    controlsMenu.start(visibleHeaderMobile ? 'visible' : 'hidden')
+  }, [controlsLinks, controlsMenu, visibleHeaderMobile])
+
+  return (
+    <motion.div
+      animate={controlsMenu}
+      variants={{
+        visible: {
+          height: 200,
+          transition: { duration: 0.3, delay: 0.1 },
+        },
+        hidden: {
+          height: 0,
+          transition: { duration: 0.3 },
+        },
+      }}
+      initial="hidden"
+      className="flex w-full items-center justify-center overflow-hidden bg-brand-mindaro text-slate-light-3 shadow-md transition-all duration-500 dark:bg-brand-indigo md:hidden"
+    >
+      <NavLinks isHiddenLinks={isHiddenLinks} controlsLinks={controlsLinks} mobile={true} />
+    </motion.div>
+  )
+}
