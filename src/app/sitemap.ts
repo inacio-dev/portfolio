59aa3d9fd4a72c1a51767929dd750b6dd47d1c2e
inacio-dev <inaciormgalvao@gmail.com>
2023-10-22 22:46:14 -0300

    fix

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -9,5 +9,55 @@ export default function sitemap(): MetadataRoute.Sitemap {
       lastModified: new Date(),
       priority: 1,
     },
+    {
+      url: `${baseUrl}/about`,
+      lastModified: new Date(),
+      priority: 0.8,
+    },
+    {
+      url: `${baseUrl}/sobre`,
+      lastModified: new Date(),
+      priority: 0.8,
+    },
+    {
+      url: `${baseUrl}/records?view=ongoing`,
+      lastModified: new Date(),
+      priority: 0.8,
+    },
+    {
+      url: `${baseUrl}/historico?view=ongoing`,
+      lastModified: new Date(),
+      priority: 0.8,
+    },
+    {
+      url: `${baseUrl}/records?view=resume`,
+      lastModified: new Date(),
+      priority: 0.8,
+    },
+    {
+      url: `${baseUrl}/historico?view=resume`,
+      lastModified: new Date(),
+      priority: 0.8,
+    },
+    {
+      url: `${baseUrl}/projects`,
+      lastModified: new Date(),
+      priority: 0.8,
+    },
+    {
+      url: `${baseUrl}/projetos`,
+      lastModified: new Date(),
+      priority: 0.8,
+    },
+    {
+      url: `${baseUrl}/terms`,
+      lastModified: new Date(),
+      priority: 0.2,
+    },
+    {
+      url: `${baseUrl}/termos`,
+      lastModified: new Date(),
+      priority: 0.2,
+    },
   ]
 }
