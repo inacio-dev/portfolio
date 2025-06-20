f6aa03269a2a678469e39cd756a5902c8570212f
inacio-dev <inaciormgalvao@gmail.com>
2025-06-20 16:17:27 -0300

    first commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,42 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
+import { FlatCompat } from '@eslint/eslintrc'
+import unusedImports from 'eslint-plugin-unused-imports'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
-});
+})
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+  ...compat.extends('next/core-web-vitals', 'next/typescript', 'prettier'),
+  {
+    plugins: {
+      'unused-imports': unusedImports,
+    },
+    rules: {
+      '@next/next/no-html-link-for-pages': 'off',
+      'react/jsx-key': 'off',
+      'tailwindcss/no-custom-classname': 'off',
+      'no-unused-vars': 'off',
+      'unused-imports/no-unused-imports': 'error',
+      'unused-imports/no-unused-vars': [
+        'warn',
+        {
+          vars: 'all',
+          varsIgnorePattern: '^_',
+          args: 'after-used',
+          argsIgnorePattern: '^_',
+        },
+      ],
+    },
+  },
+  {
+    files: ['*.ts', '*.tsx', '*.js'],
+    parser: '@typescript-eslint/parser',
+  },
+]
 
-export default eslintConfig;
+export default eslintConfig
